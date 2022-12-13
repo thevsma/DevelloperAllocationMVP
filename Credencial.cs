c5328b305334993e6f028b05c242da817f0e3478
matth <matth@DESKTOP-MVP1507>
2022-12-13 17:42:52 -0300

    Adicionar arquivos de projeto.

---
+++ Credencial.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeveloperAllocationMVP
+{
+    [Table("Credenciais")]
+    internal class Credencial
+    {
+        public Int64 Id { get; set; }
+
+        //constant to append with the password, ensuring more security
+        public const String SALT = "m4th3eus";
+
+        [Index(IsUnique = true)]
+        [StringLength(250)]
+        public String Email { get; set; }
+
+        private String _senha;
+
+        //verifies the password length, if it is higher than 8 and lower than 12, it encrypts it
+        //if the password has exactly 64 characters, it is already encrypted, so it doesn't encrypt it again
+        [Required]
+        public String Senha
+        {
+            get 
+            { 
+                return _senha;
+            }
+            set
+            {
+                if(value.Length >= 8 && value.Length <= 12)
+                {
+                    _senha = ComputeSHA256(value, SALT);
+                }
+                else if(value.Length == 64)
+                {
+                    _senha = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Senha inválida!");
+                }
+            }
+        }
+
+        public Boolean Ativo { get; set; }
+        public Boolean Admin { get; set; }
+
+        [Required]
+        public Desenvolvedor Desenvolvedor { get; set; }
+
+        public Credencial()
+        {
+
+        }
+
+        #region Hashing
+        public static String ComputeSHA256(String input)
+        {
+            return ComputeSHA256(input, null);
+        }
+
+        public static String ComputeSHA256(String input, String salt)
+        {
+            String hash = String.Empty;
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashValue = sha256.ComputeHash(Encoding.UTF8.GetBytes(salt + input));
+
+                foreach (byte b in hashValue)
+                {
+                    hash = $"{b:X2}";
+                }
+            }
+            return hash;
+        }
+        #endregion
+
+        public static Boolean Autenticar(String email, String senha)
+        {
+            Desenvolvedor dev = DevRepos.EncontrarEmail(email);
+
+            if(dev != null)
+            {
+                String senhaBd = dev.Credencial.Senha;
+                senha = ComputeSHA256(senha);
+
+                if (senha == senhaBd) return true;
+                else
+                {
+                    MessageBox.Show("SENHA INCORRETA!");
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("EMAIL NÃO EXISTENTE!");
+                return false;
+            }
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DeveloperAllocationMVP
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.pnlLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.Controls.Add(this.btnLogin);
+            this.pnlLogin.Controls.Add(this.txtSenha);
+            this.pnlLogin.Controls.Add(this.txtEmail);
+            this.pnlLogin.Controls.Add(this.lblSenha);
+            this.pnlLogin.Controls.Add(this.lblEmail);
+            this.pnlLogin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlLogin.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(288, 169);
+            this.pnlLogin.TabIndex = 0;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(66, 49);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 0;
+            this.lblEmail.Text = "Email:";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(60, 75);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(41, 13);
+            this.lblSenha.TabIndex = 1;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(107, 42);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 2;
+            this.txtEmail.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtEmail_KeyUp);
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(107, 68);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 3;
+            this.txtSenha.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtSenha_KeyUp);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(132, 94);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 169);
+            this.Controls.Add(this.pnlLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmLogin";
+            this.Text = "Login";
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLogin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblEmail;
+    }
+}
+
